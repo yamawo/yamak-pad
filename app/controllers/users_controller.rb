87b120236c76935c2c6f9cdf21227abc3cd5ffce
yamawo <y7a3m1asaki@gmail.com>
2019-09-15 20:41:25 +0900

    add index and something

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
     def show
-        @nickname = current_user.nickname
-        @tweets = current_user.tweets.page(params[:page]).per(5).order("created_at DESC")
+        user = User.find(params[:id])
+        @nickname = user.nickname
+        @tweets = user.tweets.page(params[:page]).per(5).order("created_at DESC")
     end
 end
